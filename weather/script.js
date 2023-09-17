612d5442230f303a8b661496ac00ae44272bf927
SahilHota <126983585+SahilHota@users.noreply.github.com>
2023-09-17 13:24:08 +0530

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,34 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5f9eeffa25msh99683aff8d7154bp17e4dejsn163986d56533',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+
+
+            console.log(response)
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+
+        })
+        .catch(err => console.error(err));
+}
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("delhi")
